fa815d6b45ce16bbd52c9edd5b14f548356e6d39
vishwajeet2912 <vishwajeet8261@gmail.com>
2026-01-27 00:28:38 +0530

    code update day 3

--- auth-app/src/main/java/com/Substring/auth/services/UserServiceImpl.java
+++ auth-app/src/main/java/com/Substring/auth/services/UserServiceImpl.java
@@ -1,13 +1,35 @@
 package com.Substring.auth.services;
 
 import com.Substring.auth.dtos.UserDtos;
+import com.Substring.auth.entities.Provider;
+import com.Substring.auth.entities.User;
+import com.Substring.auth.repositories.UserRespository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
+
+
+    private final UserRespository userRespository;
+    private final ModelMapper modelMapper;
+
     @Override
     public UserDtos createUser(UserDtos userDtos) {
-        return null;
+
+        if (userDtos.getEmail()== null || userDtos.getEmail().isBlank()){
+            throw new IllegalArgumentException("Email is requried");
+        }
+        if (userRespository.existsByEmail(userDtos.getEmail())){
+            throw  new IllegalArgumentException("Email already exists");
+        }
+       User user = modelMapper.map(userDtos, User.class);
+        user.setProvider(userDtos.getProvider() != null ? userDtos.getProvider() : Provider.Local);
+        User savedUser = userRespository.save(user);
+        return modelMapper.map(savedUser, UserDtos.class);
     }
 
     @Override
