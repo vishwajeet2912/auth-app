e9cc32253a676237688bd78f417c4d15d32a2650
vishwajeet2912 <vishwajeet8261@gmail.com>
2026-03-06 23:51:22 +0530

    minor code improvements

--- auth-app/src/main/java/com/Substring/auth/exceptions/GlobalExceptionHandler.java
+++ auth-app/src/main/java/com/Substring/auth/exceptions/GlobalExceptionHandler.java
@@ -8,6 +8,8 @@ import io.jsonwebtoken.JwtException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.tomcat.websocket.AuthenticationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -18,6 +20,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice // by this annotation we can handle exception of the application
 public class GlobalExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+
 @ExceptionHandler({
         UsernameNotFoundException.class,
         BadCredentialsException.class,
@@ -27,7 +33,8 @@ public class GlobalExceptionHandler {
 //        AuthenticationException.class
 })
     public ResponseEntity<ApiError> handleAuthException(Exception e, HttpServletRequest request){
-      var apierror =ApiError.of(HttpStatus.BAD_REQUEST.value(),"Bad request", e.getMessage(), request.getRequestURI());
+    logger.info("Exception :{}",e.getClass().getName());
+    var apierror =ApiError.of(HttpStatus.BAD_REQUEST.value(),"Bad request", e.getMessage(), request.getRequestURI());
       return ResponseEntity.badRequest().body(ApiError.of(HttpStatus.BAD_REQUEST.value(), "Bad request", e.getMessage(), request.getRequestURI()));
     }
 
